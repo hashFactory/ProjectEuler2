5b83f9abef47a13224cd74991de7ca2119e09bcf
Tristan Charpentier <tristan_charpentier@hotmail.com>
2018-02-19 22:45:36 -0500

    Finished 15

---
+++ src/Challenges/ProjectEuler15.java
@@ -0,0 +1,17 @@
+package Challenges;
+
+// TODO: Didn't solve computationally, look at how each down and right could be either 1 or 0
+// TODO: Then realize for 2x2 it's either
+// TODO: 1100 1010 1001 0110 0101 or 0011
+// TODO: Seem familiar? That's because you're fitting two 1s and two 0s in 4 spots
+// TODO: Conclusion? It's just 40 C 20
+
+public class ProjectEuler15
+{
+    private static int n = 20;
+
+    public static void main(String [] args)
+    {
+
+    }
+}
